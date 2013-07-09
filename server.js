bfaf2c9e389ff943c973d8a6a6b2b138f5704bfb
Harpreet <happysinghsingh7@gmail.com>
2013-07-10 04:21:51 +0530

    parsing urls pathname method addition

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
 var http = require("http");
+var url = require("url");
 
 function start() {
 	function onRequest (request, response) { 
-
-		console.log("Request Received.");
+		
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname +" Received.");
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write("Hello World!");
 		response.end();
